9c5094eaa141de0e45225c86d9597e682f1dedf6
Clemens Horn <clemens.horn@mni.thm.de>
2025-03-15 11:56:09 +0100

    feat(nix): add bonjour, w3c scripts

--- flake.nix
+++ flake.nix
@@ -28,6 +28,54 @@
       withTruststore = true;
       withDeltaUpdates = true;
     };
+    bonjour = pkgs.writeShellApplication {
+      name = "bonjour";
+      runtimeInputs = [
+        pkgs.wineWowPackages.unstableFull
+        pkgs.winetricks
+        umu
+      ];
+      text = ''
+        export WINEPREFIX=$HOME/.local/share/wineprefixes/bnet
+        export WINEARCH=win64
+        PROTON_VERB=runinprefix wine net stop 'Bonjour Service'
+        PROTON_VERB=runinprefix wine net start 'Bonjour Service'
+      '';
+    };
+    w3champions = pkgs.writeShellApplication {
+      name = "w3champions";
+      runtimeInputs = [
+        pkgs.curl
+        pkgs.samba
+        pkgs.winetricks
+        umu
+      ];
+      text = ''
+        export WINEPREFIX=$HOME/.local/share/wineprefixes/bnet
+        export WINEARCH=win64
+        export GAMEID=umu-default
+
+        install_w3c() {
+          W3C_SETUP_URL="https://update-service.w3champions.com/api/launcher/win"
+          DOWNLOAD_DIR="$(mktemp -d)"
+          W3C_SETUP_EXE="$DOWNLOAD_DIR/w3c-setup.exe"
+
+          echo "Downloading W3Champions installer..."
+          mkdir -p "$DOWNLOAD_DIR"
+          curl -L "$W3C_SETUP_URL" -o "$W3C_SETUP_EXE"
+
+          umu-run "$W3C_SETUP_EXE"
+        }
+
+        W3C_EXE="$WINEPREFIX/drive_c/users/$USER/AppData/Local/Programs/w3champions/w3champions.exe"
+
+        if [ ! -e "$W3C_EXE" ]; then
+          install_w3c
+        fi
+
+        umu-run "$W3C_EXE"
+      '';
+    };
     battlenet = pkgs.writeShellApplication {
       name = "battlenet";
       runtimeInputs = [
@@ -62,7 +110,7 @@
   in {
     packages = {
       ${system} = {
-        inherit battlenet;
+        inherit battlenet bonjour w3champions;
         default = self.packages.${system}.battlenet;
       };
     };
