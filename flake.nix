55861b20d9fc34c8ccdd0eaf869b28b83c71111e
Clemens Horn <clemens.horn@mni.thm.de>
2025-03-16 20:46:07 +0100

    fix(bonjour): add GAMEID env

--- flake.nix
+++ flake.nix
@@ -64,6 +64,7 @@
       text = ''
         export WINEPREFIX=$HOME/.local/share/wineprefixes/bnet
         export WINEARCH=win64
+        export GAMEID=umu-default
         export PROTON_VERB=runinprefix
 
         umu-run "$WINEPREFIX/drive_c/windows/system32/net.exe" stop 'Bonjour Service'
