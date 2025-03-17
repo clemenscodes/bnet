88030acb3cd98e843d267e5a0a0f0029fd07ca65
Clemens Horn <clemens.horn@mni.thm.de>
2025-03-17 15:40:48 +0100

    test

--- flake.nix
+++ flake.nix
@@ -32,15 +32,20 @@
       name = "battlenet";
       runtimeInputs = [
         pkgs.curl
-        pkgs.samba
-        pkgs.winetricks
         umu
       ];
       text = ''
         export WINEPREFIX=$HOME/.local/share/wineprefixes/bnet
         export WINEARCH=win64
+        export WINEESYNC=1
         export GAMEID=umu-default
         export BNET_EXE="$WINEPREFIX/drive_c/Program Files (x86)/Battle.net/Battle.net.exe"
+        export DXVK_HUD=compiler
+        export DXVK_STATE_CACHE_PATH=$WINEPREFIX
+        export STAGING_SHARED_MEMORY=1
+        export __GL_SHADER_DISK_CACHE=1
+        export __GL_SHADER_DISK_CACHE_SKIP_CLEANUP=1
+        export __GL_SHADER_DISK_CACHE_PATH=$WINEPREFIX
 
         install_bnet() {
           DOWNLOAD_DIR="$(mktemp -d)"
@@ -125,17 +130,14 @@
         }
 
         install_webview2() {
-          WEBVIEW2_SETUP_URL="https://msedge.sf.dl.delivery.mp.microsoft.com/filestreamingservice/files/1016fd13-5855-47b6-bfb3-f218431cf784/Microsoft.WebView2.FixedVersionRuntime.134.0.3124.68.x64.cab"
           DOWNLOAD_DIR="$(mktemp -d)"
-          WEBVIEW2_SETUP_CAB="$DOWNLOAD_DIR/webview2.cab"
           WEBVIEW2_SETUP_EXE="$DOWNLOAD_DIR/Microsoft.WebView2.FixedVersionRuntime.134.0.3124.68.x64/msedgewebview2.exe"
-          echo "Downloading WebView2 installer..."
-          curl -L "$WEBVIEW2_SETUP_URL" -o "$WEBVIEW2_SETUP_CAB"
+          WEBVIEW2_SETUP_CAB="${./assets/MicrosoftEdgeWebview2Setup.exe}"
           echo "Unpacking WebView2 cabinet files..."
           cabextract "$WEBVIEW2_SETUP_CAB" -d "$DOWNLOAD_DIR"
-          umu-run winetricks --force vcrun2017
           umu-run winetricks --force corefonts
-          umu-run winetricks --force vcrun2015
+          umu-run winetricks --force vcrun2017
+          umu-run winetricks win7
           umu-run "$WEBVIEW2_SETUP_EXE"
         }
 
